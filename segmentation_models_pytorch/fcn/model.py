0d0875dc32c141fc383ddf3eb526b656132c721a
Almat Utegulov <utegulovalmat@gmail.com>
2019-11-21 18:10:32 +0100

    add pre-commit hook

--- segmentation_models_pytorch/fcn/model.py
+++ segmentation_models_pytorch/fcn/model.py
@@ -1,9 +1,196 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import defaultdict
+import time
+import copy
+import torch.optim as optim
+
+from torchvision import datasets, transforms
 
 
 class FCN(nn.Module):
 
     def __init__(self):
-        pass
+        super(FCN, self).__init__()
+        # Defining the layers, 128, 64, 10 units each
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        # Output layer, 10 units - one for each digit
+        self.fc3 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        ''' Forward pass through the network, returns the output logits '''
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.softmax(x, dim=1)
+        return x
+
+
+def dice_loss(pred, target, smooth=1.):
+    pred = pred.contiguous()
+    target = target.contiguous()
+
+    intersection = (pred * target).sum(dim=2).sum(dim=2)
+
+    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+
+    return loss.mean()
+
+
+def calc_loss(pred, target, metrics, bce_weight=0.5):
+    bce = F.binary_cross_entropy_with_logits(pred, target)
+
+    pred = F.sigmoid(pred)
+    dice = dice_loss(pred, target)
+
+    loss = bce * bce_weight + dice * (1 - bce_weight)
+
+    metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
+    metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
+    metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
+
+    return loss
+
+
+def print_metrics(metrics, epoch_samples, phase):
+    outputs = []
+    for k in metrics.keys():
+        outputs.append("{}: {:4f}".format(k, metrics[k] / epoch_samples))
+
+    print("{}: {}".format(phase, ", ".join(outputs)))
+
+
+def view_classify(img, ps, version="MNIST"):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    if version == "MNIST":
+        ax2.set_yticklabels(np.arange(10))
+    elif version == "Fashion":
+        ax2.set_yticklabels(['T-shirt/top',
+                            'Trouser',
+                            'Pullover',
+                            'Dress',
+                            'Coat',
+                            'Sandal',
+                            'Shirt',
+                            'Sneaker',
+                            'Bag',
+                            'Ankle Boot'], size='small');
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def num_flat_features(x):
+    size = x.size()[1:]  # all dimensions except the batch dimension
+    num_features = 1
+    for s in size:
+        num_features *= s
+    return num_features
+
+
+def sequential_model():
+    # Hyperparameters for our network
+    input_size = 784
+    hidden_sizes = [128, 64]
+    output_size = 10
+    # Build a feed-forward network
+    model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                          nn.ReLU(),
+                          nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                          nn.ReLU(),
+                          nn.Linear(hidden_sizes[1], output_size),
+                          nn.Softmax(dim=1))
+    return model
+
+
+def main():
+    # Define a transform to normalize the data
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+    ])
+    # Download and load the training data
+    trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+    # Init model
+    model = FCN()
+    print(model.fc1)
+    # model = sequential_model()
+    # print(model[0])
+    # print(summary(model, input_size=(1, 64, 64)))
+
+    # Train model ====================
+
+    # freeze backbone layers
+    # for l in model.base_layers:
+    #    for param in l.parameters():
+    #        param.requires_grad = False
+
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
+    best_model = train(
+        model=model,
+        optimizer=optimizer,
+        num_epochs=1,
+        train_loader=trainloader,
+    )
+    print(best_model)
+
+
+def train(model, optimizer, num_epochs, train_loader):
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 1e10
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        since = time.time()
+
+        # Grab some data
+        for images, labels in train_loader:
+            print(type(images))
+            print(images.shape)
+            print(labels.shape)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Resize images into a 1D vector,
+            # new shape is (batch size, color channels, image pixels)
+            images.resize_(64, 1, 784)
+
+            # Forward pass through the network
+            img_idx = 0
+            ps = model.forward(images[img_idx, :])
+            print(ps)  # softmax output
+            view_classify(images[0].view(1, 28, 28), ps)
+            break
+
+    return model
+
+
+if __name__ == '__main__':
+    """
+    cd ~/Downloads/Theses/Repos/segmentation_models.pytorch
+    source ~/ml-env3/bin/activate
+    python -m segmentation_models_pytorch.fcn.model
+    """
+    main()

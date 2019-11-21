8ce1ffd44ffc061127e1472ded914cd5ec652702
Almat Utegulov <utegulovalmat@gmail.com>
2019-11-21 20:14:22 +0100

    check pre-commit

--- segmentation_models_pytorch/fcn/model.py
+++ segmentation_models_pytorch/fcn/model.py
@@ -1,18 +1,17 @@
+import copy
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
-import numpy as np
-from collections import defaultdict
-import time
-import copy
 import torch.optim as optim
-
 from torchvision import datasets, transforms
 
 
 class FCN(nn.Module):
-
     def __init__(self):
         super(FCN, self).__init__()
         # Defining the layers, 128, 64, 10 units each
@@ -22,7 +21,7 @@ class FCN(nn.Module):
         self.fc3 = nn.Linear(64, 10)
 
     def forward(self, x):
-        ''' Forward pass through the network, returns the output logits '''
+        """ Forward pass through the network, returns the output logits """
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
@@ -32,13 +31,16 @@ class FCN(nn.Module):
         return x
 
 
-def dice_loss(pred, target, smooth=1.):
+def dice_loss(pred, target, smooth=1.0):
     pred = pred.contiguous()
     target = target.contiguous()
 
     intersection = (pred * target).sum(dim=2).sum(dim=2)
 
-    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+    loss = 1 - (
+        (2.0 * intersection + smooth)
+        / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)
+    )
 
     return loss.mean()
 
@@ -51,9 +53,9 @@ def calc_loss(pred, target, metrics, bce_weight=0.5):
 
     loss = bce * bce_weight + dice * (1 - bce_weight)
 
-    metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
-    metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
-    metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
+    metrics["bce"] += bce.data.cpu().numpy() * target.size(0)
+    metrics["dice"] += dice.data.cpu().numpy() * target.size(0)
+    metrics["loss"] += loss.data.cpu().numpy() * target.size(0)
 
     return loss
 
@@ -67,30 +69,35 @@ def print_metrics(metrics, epoch_samples, phase):
 
 
 def view_classify(img, ps, version="MNIST"):
-    ''' Function for viewing an image and it's predicted classes.
-    '''
+    """ Function for viewing an image and it's predicted classes.
+    """
     ps = ps.data.numpy().squeeze()
 
-    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    fig, (ax1, ax2) = plt.subplots(figsize=(6, 9), ncols=2)
     ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
-    ax1.axis('off')
+    ax1.axis("off")
     ax2.barh(np.arange(10), ps)
     ax2.set_aspect(0.1)
     ax2.set_yticks(np.arange(10))
     if version == "MNIST":
         ax2.set_yticklabels(np.arange(10))
     elif version == "Fashion":
-        ax2.set_yticklabels(['T-shirt/top',
-                            'Trouser',
-                            'Pullover',
-                            'Dress',
-                            'Coat',
-                            'Sandal',
-                            'Shirt',
-                            'Sneaker',
-                            'Bag',
-                            'Ankle Boot'], size='small');
-    ax2.set_title('Class Probability')
+        ax2.set_yticklabels(
+            [
+                "T-shirt/top",
+                "Trouser",
+                "Pullover",
+                "Dress",
+                "Coat",
+                "Sandal",
+                "Shirt",
+                "Sneaker",
+                "Bag",
+                "Ankle Boot",
+            ],
+            size="small",
+        )
+    ax2.set_title("Class Probability")
     ax2.set_xlim(0, 1.1)
 
     plt.tight_layout()
@@ -111,24 +118,27 @@ def sequential_model():
     hidden_sizes = [128, 64]
     output_size = 10
     # Build a feed-forward network
-    model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                          nn.ReLU(),
-                          nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-                          nn.ReLU(),
-                          nn.Linear(hidden_sizes[1], output_size),
-                          nn.Softmax(dim=1))
+    model = nn.Sequential(
+        nn.Linear(input_size, hidden_sizes[0]),
+        nn.ReLU(),
+        nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+        nn.ReLU(),
+        nn.Linear(hidden_sizes[1], output_size),
+        nn.Softmax(dim=1),
+    )
     return model
 
 
 def main():
     # Define a transform to normalize the data
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)),
-    ])
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),]
+    )
     # Download and load the training data
-    trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+    trainset = datasets.MNIST(
+        "~/.pytorch/MNIST_data/", download=True, train=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True,)
 
     # Init model
     model = FCN()
@@ -144,50 +154,76 @@ def main():
     #    for param in l.parameters():
     #        param.requires_grad = False
 
-    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
+    optimizer = optim.Adam(
+        filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4
+    )
     best_model = train(
-        model=model,
-        optimizer=optimizer,
-        num_epochs=1,
-        train_loader=trainloader,
+        model=model, optimizer=optimizer, num_epochs=1, train_loader=trainloader,
     )
     print(best_model)
 
 
 def train(model, optimizer, num_epochs, train_loader):
-
-    best_model_wts = copy.deepcopy(model.state_dict())
+    best_model_weights = copy.deepcopy(model.state_dict())
     best_loss = 1e10
 
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        print("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print("-" * 10)
         since = time.time()
 
+        model.train()  # Set model to training mode
+        metrics = defaultdict(float)
+        epoch_samples = 0
+
         # Grab some data
-        for images, labels in train_loader:
-            print(type(images))
-            print(images.shape)
-            print(labels.shape)
+        for inputs, labels in train_loader:
+            print(type(inputs), inputs.shape, labels.shape)
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
-            # Resize images into a 1D vector,
+            # forward pass
+            with torch.set_grad_enabled(True):
+                outputs = model(inputs)
+                loss = calc_loss(outputs, labels, metrics)
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            epoch_samples += inputs.size(0)
+
+            # Resize inputs into a 1D vector,
             # new shape is (batch size, color channels, image pixels)
-            images.resize_(64, 1, 784)
+            inputs.resize_(64, 1, 784)
 
             # Forward pass through the network
             img_idx = 0
-            ps = model.forward(images[img_idx, :])
+            ps = model.forward(inputs[img_idx, :])
             print(ps)  # softmax output
-            view_classify(images[0].view(1, 28, 28), ps)
+            view_classify(inputs[0].view(1, 28, 28), ps)
             break
 
+            print_metrics(metrics, epoch_samples, phase)
+            epoch_loss = metrics["loss"] / epoch_samples
+
+            # deep copy the model
+            if phase == "val" and epoch_loss < best_loss:
+                print("saving best model")
+                best_loss = epoch_loss
+                best_model_weights = copy.deepcopy(model.state_dict())
+
+        time_elapsed = time.time() - since
+        print("{:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60))
+
+    print("Best val loss: {:4f}".format(best_loss))
+
+    # load best model weights
+    model.load_state_dict(best_model_weights)
     return model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     cd ~/Downloads/Theses/Repos/segmentation_models.pytorch
     source ~/ml-env3/bin/activate

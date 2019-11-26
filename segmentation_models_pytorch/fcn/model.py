c8fff4897849f0c9f5beb73a0f78b6426eb3f0f7
Almat Utegulov <utegulovalmat@gmail.com>
2019-11-26 20:47:30 +0100

    train fcn - wip

--- segmentation_models_pytorch/fcn/model.py
+++ segmentation_models_pytorch/fcn/model.py
@@ -1,18 +1,52 @@
+from typing import Optional, Union, List
+
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torchvision import models
 
 
 class FCN(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        # encoder_name: str = "resnet34",
+        # encoder_depth: int = 5,
+        # encoder_weights: str = "imagenet",
+        # decoder_use_batchnorm: bool = True,
+        # decoder_channels: List[int] = (256, 128, 64, 32, 16),
+        # decoder_attention_type: Optional[str] = None,
+        # in_channels: int = 3,
+        classes: int = 1,
+        # activation: Optional[Union[str, callable]] = None,
+        # aux_params: Optional[dict] = None,
+    ):
         super(FCN, self).__init__()
-        self.fc1 = nn.Linear(784, 64)
-        # Output layer, 10 units - one for each digit
-        self.fc3 = nn.Linear(64, 10)
+        self.base_model = models.resnet18(pretrained=True)
+
+        layers = list(self.base_model.children())
+        self.layer1 = nn.Sequential(*layers[:5])  # size=(N, 64, x.H/2, x.W/2)
+        self.upsample1 = nn.Upsample(scale_factor=4, mode="bilinear")
+        self.layer2 = layers[5]  # size=(N, 128, x.H/4, x.W/4)
+        self.upsample2 = nn.Upsample(scale_factor=8, mode="bilinear")
+        self.layer3 = layers[6]  # size=(N, 256, x.H/8, x.W/8)
+        self.upsample3 = nn.Upsample(scale_factor=16, mode="bilinear")
+        self.layer4 = layers[7]  # size=(N, 512, x.H/16, x.W/16)
+        self.upsample4 = nn.Upsample(scale_factor=32, mode="bilinear")
+
+        self.conv1k = nn.Conv2d(64 + 128 + 256 + 512, classes, 1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        """ Forward pass through the network, returns the output logits """
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc3(x)
-        x = F.log_softmax(x, dim=1)
-        return x
+        x = self.layer1(x)
+        up1 = self.upsample1(x)
+        x = self.layer2(x)
+        up2 = self.upsample2(x)
+        x = self.layer3(x)
+        up3 = self.upsample3(x)
+        x = self.layer4(x)
+        up4 = self.upsample4(x)
+
+        merge = torch.cat([up1, up2, up3, up4], dim=1)
+        merge = self.conv1k(merge)
+        out = self.sigmoid(merge)
+
+        return out
--- segmentation_models_pytorch/fcn/test_model.py
+++ segmentation_models_pytorch/fcn/test_model.py
@@ -6,98 +6,172 @@ source ~/ml-env3/bin/activate
 python -m segmentation_models_pytorch.fcn.test_model
 """
 import copy
-from collections import defaultdict
-
+import numpy as np
 import time
 import torch
+import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets, transforms
+from collections import defaultdict
 
-from .utils import view_classify
-from .utils import print_metrics
-from .utils import calc_loss
+from segmentation_models_pytorch.utils.losses import DiceLoss
+
 from .model import FCN
+from .utils import print_metrics
+from .utils import masks_to_colorimg
+from .utils import plot_side_by_side
+from .simulation import generate_random_data
 
 
 def main():
-    # Define a transform to normalize the data
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),]
-    )
-    # Download and load the training data
-    trainset = datasets.MNIST(
-        "~/.pytorch/MNIST_data/", download=True, train=True, transform=transform
-    )
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True,)
+    # Generate some random images
+    input_images, target_masks = generate_random_data(192, 192, count=3)
+    for x in [input_images, target_masks]:
+        print(x.shape)
+        print(x.min(), x.max())
 
-    testset = datasets.MNIST(
-        "~/.pytorch/MNIST_data/", download=True, train=False, transform=transform
-    )
-    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True,)
+    # Change channel-order and make 3 channels for matplot
+    # input_images_rgb = [
+    #     (x.swapaxes(0, 2).swapaxes(0, 1)  # * -255 + 255
+    #     ).astype(np.uint8) for x in input_images
+    # ]
+    # target_masks_rgb = [masks_to_colorimg(x) for x in input_images_rgb]
+    # plot_side_by_side([input_images_rgb, target_masks_rgb])
 
     # Init model
-    model = FCN()
+    device = torch.device("cpu")
+    model = FCN().to(device)
     # print(model.fc1)
 
     # Train model
+    # criterion = nn.BCELoss()
+    criterion = DiceLoss()
     optimizer = optim.Adam(
         filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4
     )
-    best_model = train(
-        model=model,
-        optimizer=optimizer,
-        num_epochs=1,
-        train_loader=trainloader,
-        valid_loader=testloader,
-    )
-    print(best_model)
+    best_model = train_model(model, criterion, optimizer, num_epochs=3)
+
+    # optimizer = optim.Adam(
+    #     filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4
+    # )
+    # criterion = nn.BCELoss()
+    # best_model = train(
+    #     model=model,
+    #     optimizer=optimizer,
+    #     criterion=criterion,
+    #     num_epochs=1,
+    # )
+    # print(best_model)
     # print(best_model.state_dict())
 
     # View prediction
-    images, labels = next(iter(testloader))
-    images.resize_(64, 1, 784)
+    images, labels = input_images, target_masks
     img_idx = 0
-    ps = best_model.forward(images[img_idx, :])
-    print(ps)  # softmax output probabilities
-    ps = torch.exp(ps)  # back to percents
-    view_classify(images[0].view(1, 28, 28), ps)
+    preds = best_model.forward(images[img_idx, :])
+    # Map each channel (i.e. class) to each color
+    target_masks_rgb = [masks_to_colorimg(x) for x in preds]
+    # Left: Input image, Right: Target mask
+    plot_side_by_side([images, target_masks_rgb])
 
 
-def train(model, optimizer, num_epochs, train_loader, valid_loader):
-    best_model_weights = copy.deepcopy(model.state_dict())
+# def train(model, optimizer, criterion, num_epochs):
+#     best_model_weights = copy.deepcopy(model.state_dict())
+#     best_loss = 1e10
+#
+#     # Freeze backbone layers
+#     # for l in model.base_layers:
+#     #    for param in l.parameters():
+#     #        param.requires_grad = False
+#
+#     device = torch.device("cpu")
+#
+#     for epoch in range(num_epochs):
+#         print("Epoch {}/{}".format(epoch, num_epochs - 1))
+#         print("-" * 10)
+#
+#         since = time.time()
+#
+#         # Each epoch has a training and validation phase
+#         for phase in ["train", "val"]:
+#             if phase == "train":
+#                 for param_group in optimizer.param_groups:
+#                     print("Learning rate", param_group["lr"])
+#                 model.train()  # Set model to training mode
+#             else:
+#                 model.eval()  # Set model to evaluate mode
+#
+#             metrics = defaultdict(float)
+#             epoch_samples = 0
+#
+#             input_images, target_masks = generate_random_data(192, 192, count=2)
+#             inputs = torch.from_numpy(input_images).float().to(device)
+#             labels = torch.from_numpy(target_masks).float().to(device)
+#
+#             # zero the parameter gradients
+#             optimizer.zero_grad()
+#
+#             # forward
+#             # track history if only in train
+#             with torch.set_grad_enabled(phase == "train"):
+#                 preds = model(inputs)
+#                 loss = criterion(preds, labels)
+#                 # metrics["loss"] += loss.data.cpu().numpy() * labels.size(0)
+#
+#                 # backward + optimize only if in training phase
+#                 if phase == "train":
+#                     loss.backward()
+#                     optimizer.step()
+#
+#             # statistics
+#             epoch_samples += inputs.size(0)
+#
+#             print_metrics(metrics, epoch_samples, phase)
+#             epoch_loss = metrics["loss"] / epoch_samples
+#
+#             # deep copy the model
+#             if phase == "val" and epoch_loss < best_loss:
+#                 print("saving best model")
+#                 best_loss = epoch_loss
+#                 best_model_weights = copy.deepcopy(model.state_dict())
+#
+#         time_elapsed = time.time() - since
+#         print("{:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60))
+#
+#     print("Best val loss: {:4f}".format(best_loss))
+#
+#     # load best model weights
+#     model.load_state_dict(best_model_weights)
+#     return model
+
+
+def train_model(model, criterion, optimizer, num_epochs=1):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
     best_loss = 1e10
 
-    # Freeze backbone layers
-    # for l in model.base_layers:
-    #    for param in l.parameters():
-    #        param.requires_grad = False
-
-    device = torch.device("cpu")
-
-    dataloaders = {
-        "train": train_loader,
-        "val": valid_loader,
-    }
     for epoch in range(num_epochs):
         print("Epoch {}/{}".format(epoch, num_epochs - 1))
         print("-" * 10)
-
-        since = time.time()
-
+        device = torch.device("cpu")
         # Each epoch has a training and validation phase
         for phase in ["train", "val"]:
             if phase == "train":
-                for param_group in optimizer.param_groups:
-                    print("LR", param_group["lr"])
                 model.train()  # Set model to training mode
             else:
                 model.eval()  # Set model to evaluate mode
 
-            metrics = defaultdict(float)
-            epoch_samples = 0
+            running_loss = 0.0
 
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.view(inputs.shape[0], -1)  # TODO: remove for conv
+            # Iterate over data.
+            batch_size = 10
+            epoch_steps = 10
+            for i in range(epoch_steps):
+                input_images, target_masks = generate_random_data(
+                    192, 192, count=batch_size
+                )
+                print(input_images.shape, target_masks.shape)
+                inputs = torch.from_numpy(input_images).float()
+                labels = torch.from_numpy(target_masks).float()
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
@@ -107,8 +181,9 @@ def train(model, optimizer, num_epochs, train_loader, valid_loader):
                 # forward
                 # track history if only in train
                 with torch.set_grad_enabled(phase == "train"):
-                    logps = model(inputs)
-                    loss = calc_loss(logps, labels, metrics)
+                    outputs = model(inputs)
+                    print("outputs shape", outputs.shape)
+                    loss = criterion(outputs, labels)
 
                     # backward + optimize only if in training phase
                     if phase == "train":
@@ -116,24 +191,29 @@ def train(model, optimizer, num_epochs, train_loader, valid_loader):
                         optimizer.step()
 
                 # statistics
-                epoch_samples += inputs.size(0)
+                running_loss += loss.item() * inputs.size(0)
 
-            print_metrics(metrics, epoch_samples, phase)
-            epoch_loss = metrics["loss"] / epoch_samples
+            epoch_loss = running_loss / (batch_size * epoch_steps)
+
+            print("{} Loss: {:.4f}".format(phase, epoch_loss))
 
             # deep copy the model
             if phase == "val" and epoch_loss < best_loss:
-                print("saving best model")
                 best_loss = epoch_loss
-                best_model_weights = copy.deepcopy(model.state_dict())
+                best_model_wts = copy.deepcopy(model.state_dict())
 
-        time_elapsed = time.time() - since
-        print("{:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60))
+        print()
 
+    time_elapsed = time.time() - since
+    print(
+        "Training complete in {:.0f}m {:.0f}s".format(
+            time_elapsed // 60, time_elapsed % 60
+        )
+    )
     print("Best val loss: {:4f}".format(best_loss))
 
     # load best model weights
-    model.load_state_dict(best_model_weights)
+    model.load_state_dict(best_model_wts)
     return model
 
 

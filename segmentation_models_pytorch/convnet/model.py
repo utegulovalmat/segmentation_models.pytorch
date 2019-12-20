e18d6db651642811e44a391bacf3448544d6b966
Almat Utegulov <utegulovalmat@gmail.com>
2019-12-21 00:33:59 +0100

    tall model cbr

--- segmentation_models_pytorch/convnet/model.py
+++ segmentation_models_pytorch/convnet/model.py
@@ -48,14 +48,23 @@ class ConvNet(nn.Module):
         # aux_params: Optional[dict] = None,
     ):
         super(ConvNet, self).__init__()
-        self.conv1 = Conv2dReLU(
-            in_channels, 64, kernel_size=3, stride=1, padding=1, use_batchnorm=True
+        # TODO: encoder_depth
+        self.conv32 = Conv2dReLU(
+            in_channels, 32, kernel_size=3, stride=1, padding=1, use_batchnorm=True
         )
-        self.conv2 = Conv2dReLU(
+        self.conv64 = Conv2dReLU(
+            32, 64, kernel_size=3, stride=1, padding=1, use_batchnorm=True
+        )
+        self.conv128 = Conv2dReLU(
             64, 128, kernel_size=3, stride=1, padding=1, use_batchnorm=True
         )
-        # TODO: encoder_depth more
-        self.conv_last = nn.Conv2d(128, classes, kernel_size=1, stride=1)
+        self.conv256 = Conv2dReLU(
+            128, 256, kernel_size=3, stride=1, padding=1, use_batchnorm=True
+        )
+        self.conv512 = Conv2dReLU(
+            256, 512, kernel_size=3, stride=1, padding=1, use_batchnorm=True
+        )
+        self.conv_last = nn.Conv2d(512, classes, kernel_size=1, stride=1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -69,8 +78,11 @@ class ConvNet(nn.Module):
         # up4 = self.upsample4(x)
         # merge = torch.cat([up1, up2, up3, up4], dim=1)
         # merge = self.conv1k(merge)
-        x = self.conv1(x)
-        x = self.conv2(x)
+        x = self.conv32(x)
+        x = self.conv64(x)
+        x = self.conv128(x)
+        x = self.conv256(x)
+        x = self.conv512(x)
         x = self.conv_last(x)
         out = self.sigmoid(x)
         return out
